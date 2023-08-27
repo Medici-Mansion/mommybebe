0c280e3b4ee90c79da6cb477ff11624c495de6fb
yunna <homer_j@hanmail.net>
2023-08-28 01:35:35 +0900

    update : global css 변경 (main 태그전체에 max-width 값 지정)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from './page.module.css';
 
 export default function HomePage() {
   return (
-    <main className={styles.main}>
+    <main>
       <div className={styles.container}>
         <Image
           src='/logo.svg'
