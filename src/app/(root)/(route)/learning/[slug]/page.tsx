95059d953141b8c6736e52d3fec93de6398a9788
yunna <homer_j@hanmail.net>
2023-08-31 18:25:18 +0900

    add : word card 페이지 라우터

---
+++ src/app/(root)/(route)/learning/[slug]/page.tsx
@@ -0,0 +1,15 @@
+type Props = {
+  params: {
+    slug: string
+  }
+}
+
+const WordCardpage = ({ params }: Props) => {
+  return (
+    <div>
+      <h1>워드 카드 페이지 입니다</h1>
+    </div>
+  )
+}
+
+export default WordCardpage
--- src/app/(root)/(route)/learning/components/select-form.tsx
+++ src/app/(root)/(route)/learning/components/select-form.tsx
@@ -1,22 +1,38 @@
 'use client'
-// import styles from '@/app/styles/CommonStyles.module.css'
+import { useRouter } from 'next/navigation'
 import styles from './select-form.module.css'
+import { useState } from 'react'
 
 const SelectCategoryForm = () => {
+  const router = useRouter()
+  const [selected, setSelected] = useState(false)
+
+  const handleNextclick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    router.push(`/learning/animals`)
+  }
+
+  const handleSelectClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log('!')
+    setSelected(true)
+  }
+
   return (
     <form>
       <div className={styles.selectBtnContainer}>
         <div className={styles.flexContainer}>
-          <div className={styles.selectBtn}>Animal</div>
-          <div className={styles.unselectBtn}>Animal</div>
+          <div className={styles.selectBtn}>Animals</div>
+          <div className={styles.unselectBtn}>Jobs</div>
         </div>
         <div className={styles.flexContainer}>
-          <div className={styles.unselectBtn}>Animal</div>
-          <div className={styles.unselectBtn}>Animal</div>
+          <div className={styles.unselectBtn}>Colors</div>
+          <div className={styles.unselectBtn}>Fruits</div>
         </div>
       </div>
       <div className={styles.nextBtnWrapper}>
-        <button className={styles.nextBtn}>Next</button>
+        <button className={styles.nextBtn} onClick={handleNextclick}>
+          Next
+        </button>
       </div>
     </form>
   )
